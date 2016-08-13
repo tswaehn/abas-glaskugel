7c9818c5c85ea17fa176d1d7a27553ddfff631f1
Sven Ginka <sven.ginka@gmail.com>
2016-08-14 00:10:37 +0200

    mod: re-implementation of <verwendung>

--- article/renderVerwendung.php
+++ article/renderVerwendung.php
@@ -1,7 +1,100 @@
 <?php
+  
 
+  function findRecursiveParents( $allLinks, &$subTree ){
+    
+    if (empty($subTree)){
+      return;
+    }
+    
+    foreach( $subTree as $article=>$array ){
+      if (isset($allLinks[$article])){
+        $subArticles= $allLinks[$article];
+        if (is_array($subArticles)){
+          foreach ($subArticles as $subArticle){
+            $subTree[$article][$subArticle]= array();
+            findRecursiveParents( $allLinks, $subTree[$article]);        
+          }
 
+        }
+      }
+    }
+    
+  }
+  
+  function renderRecursive( $articles, $tree ){
+
+      // 
+    echo '<ul class="list">';
+    foreach ($tree as $article_id=> $array){
+      if (!empty($array)){
+        $click= '<span class="Collapsable" ><a>+</a>';
+      } else {
+        $click= '<span class="NotCollapsable" >';
+      }
+      $link= '<a href="?action=article&article_id='.$article_id.'">'.$articles[$article_id]["nummer"].'</a>';
+      
+      echo '<li>'.$click.' '.$link.' '.$articles[$article_id]["such"].'</span>';
+      renderRecursive($articles, $array);
+      echo '</li>';
+    }
+    echo "</ul>";
+  }
+  
   function renderVerwendungen( $article ){
+
+    div("", "articleview");
+    disp('<span id="caption">Verwendung</span><br>');
+    
+      // get all production list entries
+      $sql= "SELECT `list_nr`, `article_id`, `elem_id` FROM ".q(DB_PRODUCTION_LIST)." WHERE 1 ORDER BY `article_id` ASC";
+      $result= dbExecute($sql);
+      // load them into an array
+      $allLinks=array();
+      foreach ($result as $item){
+        $parent= $item["article_id"];
+        $child= $item["elem_id"];
+        $allLinks[$child][]= $parent;
+      }
+      
+      // now generate the tree
+      $tree= array( $article["article_id"]=> array() );
+      findRecursiveParents($allLinks, $tree);
+
+      // get all article infos
+      $sql= "SELECT `article_id`,`nummer`,`such` FROM `gk_article` WHERE 1";
+      $result= dbExecute($sql);
+      $articles= array();
+      foreach ($result as $item){
+        $article_id= $item["article_id"];
+        $such= $item["such"];
+        $nummer= $item["nummer"];
+        $articles[$article_id]= array("nummer"=>$nummer, "such"=>$such);
+      }
+      
+
+      renderRecursive( $articles, $tree );                  
+
+            echo "<script>
+                $('.Collapsable').click(function () {
+
+                    $(this).parent().children().toggle();
+                    $(this).toggle();
+
+                });
+
+                $('.Collapsable').each(function(){
+
+                        $(this).parent().children().toggle();
+                        $(this).toggle();
+                });
+        </script>";
+            
+    
+    ediv();
+  }
+
+  function renderVerwendungenX( $article ){
     div("", "articleview");
     disp('<span id="caption">Verwendung</span><br>');
     
